2c8575c1bab8bef91d36c0431257d940086bc2e6
Leonardo Otero <leonardo.otero390@gmail.com>
2022-05-21 12:48:53 -0400

    chore: deploy env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import AuthValues from "../interfaces/AuthValues";
 
+const baseURL =
+  process.env.REACT_APP_API_URL === "prod"
+    ? "http://ec2-3-95-67-212.compute-1.amazonaws.com/"
+    : "http://localhost:5000";
+
 export const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL,
 });
 
 export const createAuthHeader = (token: string) => {
